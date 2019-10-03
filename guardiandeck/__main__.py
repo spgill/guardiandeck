14dca1e14c36d67cd513ea987c57fd4681039862
Samuel P. Gillispie II <Samuel.Gillispie@ibm.com>
2019-10-03 14:00:40 -0500

    Restructuring the package a bit

--- guardiandeck/__main__.py
+++
@@ -1,3 +0,0 @@
-from guardiandeck.deck import GuardianDeck
-
-d = GuardianDeck()
---
+++ guardiandeck/main.py
@@ -0,0 +1,9 @@
+from guardiandeck.deck import GuardianDeck
+
+
+def main():
+    GuardianDeck()
+
+
+if __name__ == "__main__":
+    main()
