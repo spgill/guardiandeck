a8172c906705d250eff3fec8010c5f28493966cf
Samuel P. Gillispie II <Samuel.Gillispie@ibm.com>
2019-10-03 14:00:11 -0500

    Hardcoding the uuid now

--- guardiandeck/config.py
+++ guardiandeck/config.py
@@ -3,6 +3,7 @@ from spgill.util.chassis import Chassis
 
 
 chassis = Chassis(
+    uuid="68da549c-5018-4c02-b6a6-1786ef16275a",
     proxy="~/.guardiandeck.json",
     props={
         # Use config
